1f296608051c80340ee5db49695b7d91aeb51e50
xepheris <auszubildender1@Auszubildender1s-iMac.local>
2017-03-10 15:57:53 +0100

    und

---
+++ js/update.js
@@ -0,0 +1,48 @@
+function update(str) {
+	var inspect_container = document.getElementsByClassName("inspect");
+
+	for (var i = 0; i < inspect_container.length; i++) {
+		inspect_container[i].style.opacity = "0.4";
+		inspect_container[i].style.filter = "alpha(opacity=40)";
+
+		var all_active_sublinks = inspect_container[i].getElementsByTagName("a");
+		for (var k = 0; k < all_active_sublinks.length; k++) {
+			all_active_sublinks[k].style.pointerEvents = "none";
+		}
+
+		var all_active_selects = inspect_container[i].getElementsByTagName("select");
+		for (var k = 0; k < all_active_selects.length; k++) {
+			all_active_selects[k].style.pointerEvents = "none";
+		}
+	}
+
+	var patience_container = document.getElementById("patience");
+	patience_container.style.display = "block";
+	patience_container.style.opacity = "1.0";
+
+	var wowhead_container = document.getElementsByClassName("wowhead-tooltip");
+	for (var i = 0; i < wowhead_container.length; i++) {
+		if (wowhead_container[i]) {
+			wowhead_container[i].parentNode.removeChild(wowhead_container[i]);
+		}
+	}
+
+	$.ajax({
+		type: "GET",
+		dataType: "html",
+		url: "var/ajax/update.php",
+		data: {
+			character: +str
+		},
+		success: function (data) {
+			var patience_text = document.getElementById("patience_text");
+			patience_text.style.display = "none";
+
+			$("#answer").html(data);
+
+			setTimeout(function () {
+				location.reload();
+			}, 5000);
+		}
+	});
+};
---
+++ var/compare/c2.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<div style="width: 30%; height: auto; padding-top: 15px; padding-bottom: 15px; float: left; background-color: #84724E; box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -moz-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -webkit-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); margin-top: 15px;">hi</div>';
+
+echo '<div style="width: 30%; height: auto; padding-top: 15px; padding-bottom: 15px; float: left; background-color: #84724E; box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -moz-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -webkit-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); margin-top: 15px; margin-left: 5%;">hi</div>';
+
+echo '<div style="width: 30%; height: auto; padding-top: 15px; padding-bottom: 15px; float: left; background-color: #84724E; box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -moz-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); -webkit-box-shadow: 0px 10px 35px 10px rgba(0,0,0,0.5); margin-top: 15px; margin-left: 5%;">hi</div>';
+
+
+?>
