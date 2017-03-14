a8da60e76ce0893bbcfcad4be0d0407629cef24c
xepheris <auszubildender1@Auszubildender1s-iMac.local>
2017-03-14 17:00:38 +0100

    upd

--- var/nav.php
+++ var/nav.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<div style="width: 100%; height: auto; padding-top: 15px; padding-bottom: 15px; text-align: center; float: left; background-color: darkslategrey; box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5);  -moz-box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5); -webkit-box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5);">';
+echo '<div style="width: 100%; height: auto; padding-top: 15px; text-align: center; float: left; background-color: darkslategrey; box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5);  -moz-box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5); -webkit-box-shadow: 0px 10px 5px 10px rgba(0,0,0,0.5);">';
 
 	// LOGGED OUT
 	if(!isset($_SESSION['guild'])) {
@@ -30,17 +30,72 @@ echo '<div style="width: 100%; height: auto; padding-top: 15px; padding-bottom: 
 	elseif(isset($_SESSION['guild'])) {
 		echo '<a href="http://artifactpower.info/dev/"><span style="color: orange; font-size: 26px; text-transform: uppercase;">' .$_SESSION['guild']. ' (' .$_SESSION['region']. '-' .$_SESSION['realm']. ')</span></a>
 		<br />
-		<br />
 		<span style="font-size: 18px; color: white;">
-		<ul>
-		<li><a href="">Update</a> |</li>	
-		<li><a href="?import">Import</a> |</li>
-		<li><a href="?inspect">Inspect</a> |</li>
-		<li><a href="?compare">Compare</a> |</li>
-		<li><a href="?share">Share</a> |</li>
-		<li><a href="?contact">Contact</a> |</li>
-		<li><a href="?logout">Logout</a></li>
-		</ul>
+		<div class="nav">';
+		
+		// IMPORT SWAP		
+		if(isset($_GET['import'])) {
+			$import = '
+			<a href="?import">IMPORT</a>';
+		}
+		elseif(!isset($_GET['import'])) {
+			$import = '
+			<a href="?import">IMPORT</a>';
+		}
+		
+		echo $import;
+		
+		// INSPECT SWAP
+		if(isset($_GET['inspect'])) {
+			$inspect = '
+			<a href="?inspect">INSPECT</a>';
+		}
+		elseif(!isset($_GET['inspect'])) {
+			$inspect = '
+			<a href="?inspect">INSPECT</a>';
+		}
+		
+		echo $inspect;
+		
+		// COMPARE SWAP
+		if(isset($_GET['compare']) || isset($_GET['source'])) {
+			$compare = '
+			<a href="?compare">COMPARE</a>';
+		}
+		elseif(!isset($_GET['compare']) && !isset($_GET['source'])) {
+			$compare = '
+			<a href="?compare">COMPARE</a>';
+		}
+		
+		echo $compare;
+		
+		// SHARE SWAP
+		if(isset($_GET['share'])) {
+			$share = '
+			<a href="?share">SHARE</a>';
+		}
+		elseif(!isset($_GET['compare'])) {
+			$share = '
+			<a href="?share">SHARE</a>';
+		}
+		
+		echo $share;
+		
+		// CONTACT SWAP
+		if(isset($_GET['contact'])) {
+			$contact = '
+			<a href="?contact">CONTACT</a>';
+		}
+		elseif(!isset($_GET['compare'])) {
+			$contact = '
+			<a href="?contact">CONTACT</a>';
+		}
+		
+		echo $contact;
+		
+		echo '
+		<a href="?logout">LOGOUT</a>
+		</div>
 		</span>';
 	}
 echo '</div>';
